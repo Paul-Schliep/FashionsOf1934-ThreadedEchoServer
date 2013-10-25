6e042def3b81340bab1773fe4835f1d44cf72268
David Andrew Pagels <pagel093@normandy.morris.umn.edu>
2013-10-25 14:17:41 -0500

    start of client-server

--- src/echoserver/EchoClient.java
+++ src/echoserver/EchoClient.java
@@ -1,4 +1,23 @@
-package echoserver;
+import java.net.*;
+import java.io.*;
 
 public class EchoClient {
+	public static void main(String [] args){
+		try{
+			Socket socket = new Socket(args[0], 6013);
+			OutputStream output = socket.getOutputStream();
+			byte [] ch = new byte[1];
+			while(System.in.read(ch) != 0){
+				output.write(ch);
+
+			}
+				
+			BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			System.out.println(reader);
+			socket.close();
+		}catch(IOException ioe){
+			System.out.println("IOException");
+			System.err.println(ioe);
+		}
+	}
 }
--- src/echoserver/EchoServer.java
+++ src/echoserver/EchoServer.java
@@ -1,4 +1,23 @@
-package echoserver;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.io.*;
 
 public class EchoServer {
+	public static void main(String [] args){
+		try{
+			ServerSocket sock = new ServerSocket(6013);
+
+			while(true){
+				Socket client = sock.accept();
+				InputStream input = client.getInputStream();
+				OutputStream output = client.getOutputStream();
+
+				output.write(input.read());
+				client.close();
+			}
+			
+		} catch(IOException ioe) {
+			System.err.println(ioe);
+		}
+	}
 }
