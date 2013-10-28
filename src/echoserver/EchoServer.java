13edf20f21bd82bea019c2b00b3fa2932fdac250
Nic McPhee <mcphee@morris.umn.edu>
2013-10-28 14:55:04 -0500

    Revert "Merge pull request #5 from DavidPagels/master"
    This reverts commit 685aba064e71340d3b44f38d5d2729033ffc4336, reversing
    changes made to 845ead84c231b9a3ec6bd16ed46cefe7105f737a.

--- src/echoserver/EchoServer.java
+++ src/echoserver/EchoServer.java
@@ -1,23 +1,4 @@
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.io.*;
+package echoserver;
 
 public class EchoServer {
-	public static void main(String [] args){
-		try{
-			ServerSocket sock = new ServerSocket(6013);
-
-			while(true){
-				Socket client = sock.accept();
-				InputStream input = client.getInputStream();
-				OutputStream output = client.getOutputStream();
-
-				output.write(input.read());
-				client.close();
-			}
-			
-		} catch(IOException ioe) {
-			System.err.println(ioe);
-		}
-	}
 }
