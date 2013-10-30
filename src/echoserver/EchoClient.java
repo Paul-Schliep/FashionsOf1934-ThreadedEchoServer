5a1540e76ce6454f1121d02d9b2521cf9ef64af4
Paul-Schliep <schli202@morris.umn.edu>
2013-10-29 19:52:27 -0500

    Got 2 of the 6 tests to pass.  The 3rd text test had a timeout error and the last 3 were breaking as expected.

--- src/echoserver/EchoClient.java
+++ src/echoserver/EchoClient.java
@@ -1,4 +1,33 @@
 package echoserver;
 
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+
+import org.omg.CORBA.portable.InputStream;
+
 public class EchoClient {
+    public static void main(String[] args) {
+        try {
+            Socket socket = new Socket("serenity.morris.umn.edu", 6013);
+            // You can change 127.0.0.1 to a machine name if you want to try this across
+            // the network to another machine.
+            // Socket socket = new Socket("avenger.morris.umn.edu", 6013);
+
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+            
+            String string;
+            while ((string = stdIn.readLine()) != null) {
+                out.println(string);
+                System.out.println(in.readLine());
+            }
+            
+            //socket.close();
+        } catch (IOException ioe) {
+            System.out.println("We caught an exception");
+            System.err.println(ioe);
+        }
+    }
 }
--- src/echoserver/EchoServer.java
+++ src/echoserver/EchoServer.java
@@ -1,4 +1,32 @@
 package echoserver;
 
+import java.net.*;
+import java.io.*;
+
 public class EchoServer {
+    public static void main(String[] args) {
+        try {
+            ServerSocket socket = new ServerSocket(6013);
+            
+            while (true) {
+                Socket client = socket.accept();
+                
+                PrintWriter out = new PrintWriter(client.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+                
+                String string;
+                while((string = in.readLine()) != null) {
+                    out.println(string);
+                    //System.out.println(string/*in.readLine()*/);
+                    if (string.equals("Fairwell")) {
+                        break;
+                    }
+                }
+                
+                //client.close();
+            }
+        } catch (IOException ioe) {
+            System.err.println(ioe);
+        }
+    }
 }
