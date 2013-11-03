31ae2f5828087b45704817f7e3c530557aab8838
Paul-Schliep <schli202@morris.umn.edu>
2013-11-02 22:30:15 -0500

    Haven't ran tests, but most should be finished based off of googling and talent :)

--- src/echoserver/EchoClient.java
+++ src/echoserver/EchoClient.java
@@ -4,32 +4,58 @@ import java.io.*;
 import java.net.*;
 
 public class EchoClient {
-    public static void main(String[] args) {
-        try {
-        	String host = "localhost";
-        	if (args.length != 0) {
-        		host = args[0];
-        	}
+    public static void main(String[] args) throws UnknownHostException, IOException, InterruptedException {
+            String host = "localhost";
+            if (args.length != 0) {
+                host = args[0];
+            }
             Socket socket = new Socket(host, 6013);
-            // You can change 127.0.0.1 to a machine name if you want to try this across
-            // the network to another machine.
-            // Socket socket = new Socket("avenger.morris.umn.edu", 6013);
-
-            OutputStream out = socket.getOutputStream();
+            Thread threadServer = new Thread(new serverWriter(socket));
+            threadServer.start();
             InputStream in = socket.getInputStream();
-            
-            
+
             int inByte;
             while ((inByte = System.in.read()) != -1) {
-                out.write(inByte);
                 System.out.write(in.read());
-                System.out.flush();
+
             }
-            
+            System.out.flush();
+            socket.shutdownOutput();
             socket.close();
-        } catch (IOException ioe) {
-            System.out.println("We caught an exception");
-            System.err.println(ioe);
+    }
+    private static class serverWriter extends Thread {
+        Socket socket;
+
+        public serverWriter(Socket socket) {
+            this.socket = socket;
+        }
+
+        public void run(){
+
+            try {
+                OutputStream out = socket.getOutputStream();
+                
+                int inByte;
+                while((inByte = System.in.read()) != -1) {
+                    out.write(inByte);
+                }
+                out.flush();
+                socket.shutdownOutput();
+               // client.close();
+
+            } catch (IOException ioe) {
+                System.err.println(ioe);
+            }
+
+            //Finally necessary?
+            finally {
+                try {
+                    socket.close();
+                } catch(Exception e) {
+                    e.printStackTrace();
+                }
+            }
+
         }
     }
 }
--- src/echoserver/EchoServer.java
+++ src/echoserver/EchoServer.java
@@ -1,28 +1,55 @@
 package echoserver;
 
 import java.net.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.io.*;
 
 public class EchoServer {
-    public static void main(String[] args) {
-        try {
-            ServerSocket socket = new ServerSocket(6013);
-            
-            while (true) {
-                Socket client = socket.accept();
-                
+    public static void main(String[] args) throws UnknownHostException, IOException, InterruptedException {
+        ServerSocket server = new ServerSocket(6013);
+        ExecutorService threadPool = Executors.newCachedThreadPool();
+
+        while(true) {
+            Socket client = server.accept();
+            threadPool.submit(new clientWriter(client));
+        }
+
+    }
+
+    private static class clientWriter extends Thread {
+        Socket client;
+
+        public clientWriter(Socket client) {
+            this.client = client;
+        }
+
+        public void run(){
+            try {
                 OutputStream out = client.getOutputStream();
                 InputStream in = client.getInputStream();
-                
+
                 int inByte;
                 while((inByte = in.read()) != -1) {
                     out.write(inByte);
                 }
-                
-                client.close();
+                out.flush();
+                client.shutdownOutput();
+               // client.close();
+
+            } catch (IOException ioe) {
+                System.err.println(ioe);
             }
-        } catch (IOException ioe) {
-            System.err.println(ioe);
+
+            //Finally necessary?
+            finally {
+                try {
+                    client.close();
+                } catch(Exception e) {
+                    e.printStackTrace();
+                }
+            }
+
         }
     }
 }
